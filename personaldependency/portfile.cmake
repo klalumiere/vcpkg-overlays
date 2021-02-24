24518e6a5b0abc665d58ee1b73ba7316074f77b4
Kevin Lalumiere <klalumiere@coveo.com>
2021-02-24 17:56:55 -0500

    Copy personalDependency

---
+++ personaldependency/portfile.cmake
@@ -0,0 +1,3 @@
+# Required, otherwise vcpkg complains that the port is invalid
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/copyright.txt DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/copyright DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
